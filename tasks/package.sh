208c7a520ff3bc5eb65da4b5c3919820672c2163
Viorel Zidariu <viorelz@gmail.com>
2021-05-27 11:55:53 +0100

    initial commit
    Signed-off-by: Kalugerov Kostadinov Kristiyan <kkalugerov@gmail.com>

---
+++ tasks/package.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+apt update
+apt install -y nodejs
+wget -O package.zip https://github.com/pivotal/postfacto/releases/download/4.3.11/package.zip
+unzip package.zip
+cd package/assets
+rm -rf Gemfile.lock
+sed -i "s/ruby '2.7.3'/ruby '2.7.2'/g" Gemfile
+gem install bundler -v 2.2.7
+bundle _2.2.7_ install
+bundle _2.2.7_ package --all-platforms
+./bin/rails assets:precompile
