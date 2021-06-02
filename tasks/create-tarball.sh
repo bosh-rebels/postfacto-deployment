37494cb333db781c71d1efc1d0538130041e5966
Kristiyan Kalugerov <kkalugerov@gmail.com>
2021-06-02 18:07:24 +0300

    pipeline cleanup
    Signed-off-by: Viorel ZP <viorelz@gmail.com>
    Co-authored-by: Viorel ZP <viorelz@gmail.com>
    Co-authored-by: Panagiotis Xynos <panos@onlyascii.io>

--- tasks/create-tarball.sh
+++ tasks/create-tarball.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 version=$(cat postfacto-version/version)
-digest=$(cat postfacto-docker-image/digest)
 
-echo "Creating postfacto package tarball with digest ${digest}..."
+echo "Creating postfacto tarball with version ${version}..."
 image_name="postfacto-package-${version}.tgz"
 mkdir -p ./output
 tar -czf "./output/${image_name}" /package
