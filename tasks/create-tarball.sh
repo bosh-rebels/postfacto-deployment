0e13552436c0c875b403a986fe9ed2d22e4d9052
Kristiyan Kalugerov <kkalugerov@gmail.com>
2021-06-02 16:16:04 +0300

    Remove digest from image name
    Signed-off-by: Viorel ZP <viorelz@gmail.com>
    Co-authored-by: Viorel ZP <viorelz@gmail.com>
    Co-authored-by: Panagiotis Xynos <panos@onlyascii.io>

--- tasks/create-tarball.sh
+++ tasks/create-tarball.sh
@@ -5,6 +5,6 @@ version=$(cat postfacto-version/version)
 digest=$(cat postfacto-docker-image/digest)
 
 echo "Creating postfacto package tarball with digest ${digest}..."
-image_name="postfacto-package-${version}-${digest}.tgz"
+image_name="postfacto-package-${version}.tgz"
 mkdir -p ./output
 tar -czf "./output/${image_name}" /package
