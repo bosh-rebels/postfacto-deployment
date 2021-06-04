005f59d33244c903473f9205c63cda7e45347ab8
Viorel Zidariu <viorelz@gmail.com>
2021-06-04 17:25:14 +0300

    add cf delete app and service job
    Signed-off-by: Kalugerov Kostadinov Kristiyan <kkalugerov@gmail.com>

---
+++ tasks/delete.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+om_cli="om-linux -k -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD"
+
+cf_guid=$(${om_cli} curl --path /api/v0/deployed/products | jq -r '.[] | select(.type == "cf") | .guid')
+cf_sys_domain=$(${om_cli} curl --path /api/v0/staged/products/${cf_guid}/properties \
+                | jq -r '.properties[".cloud_controller.system_domain"].value')
+
+cf_user=$($om_cli credentials -p cf -c .uaa.admin_credentials -f identity)
+cf_password=$($om_cli credentials -p cf -c .uaa.admin_credentials -f password)
+
+# Create org and space where nozzle is deployed
+set +x
+cf api api.${cf_sys_domain} --skip-ssl-validation
+cf auth ${cf_user} ${cf_password}
+set -x
+
+cf target -o demo -s demo
+cf delete -rf $APP_NAME
+cf delete-service -f postfacto-db
