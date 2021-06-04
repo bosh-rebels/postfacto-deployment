afceba785e19ed5b43feff1ec1c8426fdb29a632
Viorel Zidariu <viorelz@gmail.com>
2021-06-04 18:15:22 +0300

    fix script paths
    Signed-off-by: Kalugerov Kostadinov Kristiyan <kkalugerov@gmail.com>

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euf -o pipefail
-source ./cf-lib.sh
+source postfacto-deployment-code/tasks/cf-lib.sh
 
 cf_auth "$OPSMAN_DOMAIN_OR_IP_ADDRESS" "$OPSMAN_USERNAME" "$OPSMAN_PASSWORD"
 cd package-tarball/package/tas/
